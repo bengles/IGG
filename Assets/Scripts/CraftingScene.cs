aaae11706aea5119927f64943d00161e033ca521
Johan <josjobl@kth.se>
2016-11-16 22:50:14 +0100

    Add (horrible) CraftingHut scene

---
+++ Assets/Scripts/CraftingScene.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class CraftingScene : MonoBehaviour {
+
+    public Toggle enchantedTurnip;
+    public Canvas craftComplete;
+
+	// Use this for initialization
+	void Start () {
+        craftComplete = craftComplete.GetComponent<Canvas>();
+        craftComplete.enabled = false;
+	}
+	
+    public void CraftingFunction()
+    {
+        craftComplete.enabled = true;
+    }
+
+}

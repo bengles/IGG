e3d7266e57b3c0f6ab03534e61095dbccb4f13b5
Björn Englesson <engles@kth.se>
2016-11-29 15:05:49 +0100

    Added parallax effect

---
+++ Assets/Scripts/ParallaxBackground.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParallaxBackground : MonoBehaviour {
+
+	public float offsetMovement = 0.05f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if( Input.GetAxis( "Horizontal" ) < 0 )
+		{
+			this.gameObject.transform.position = new Vector3( this.gameObject.transform.position.x + offsetMovement, this.gameObject.transform.position.y, this.gameObject.transform.position.z);
+		}
+		else if( Input.GetAxis( "Horizontal" ) > 0 )
+		{
+			this.gameObject.transform.position = new Vector3( this.gameObject.transform.position.x - offsetMovement, this.gameObject.transform.position.y, this.gameObject.transform.position.z);
+		}
+	}
+}

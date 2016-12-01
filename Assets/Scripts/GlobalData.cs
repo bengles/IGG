4db0ed1744dace05f00c4c89a186099c6c85165f
Johan <josjobl@kth.se>
2016-12-01 02:19:46 +0100

    Bugfix GlobalData

--- Assets/Scripts/GlobalData.cs
+++ Assets/Scripts/GlobalData.cs
@@ -1,15 +1,54 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
-public class GlobalData : MonoBehaviour {
+public static class GlobalData {
 
-	// Use this for initialization
-	void Start () {
-	
+    public static List<ItemInfo> currentInventory;
+    public static int equippedStaffID;
+    public static int equippedBombID;
+    public static int equippedPotionID;
+    
+	public static void init() {
+        currentInventory = new List<ItemInfo>();
+        currentInventory.Add(new ItemInfo(4)); //add WinterMint
+        currentInventory.Add(new ItemInfo(5)); //add MammothBone
+
+        equippedStaffID = 1;
+        equippedBombID = 2;
+        equippedPotionID = 3;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+}
+
+public class ItemInfo
+{
+    private string itemName;
+    private string flavorText;
+    private int itemID;
+    //dmg, etc?
+
+    public ItemInfo(int id)
+    {
+        itemID = id;
+        switch(id)
+        {
+            case 4:
+                itemName = "Winter Mint";
+                flavorText = "";
+                break;
+            default:
+                Debug.Log("ItemInfo got bogus id?");
+                break;
+        }
+    }
+
+    public string getName()
+    {
+        return itemName;
+    }
+
+    public int getID()
+    {
+        return itemID;
+    }
 }

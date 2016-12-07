09a017933a46dcdffa4a73d3125890fe1c247296
Johan <josjobl@kth.se>
2016-12-07 17:58:37 +0100

    First version crafting-UI (minor bugs)

--- Assets/Scripts/CraftingScene.cs
+++ Assets/Scripts/CraftingScene.cs
@@ -2,9 +2,11 @@
 using UnityEngine.UI;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 
 public class CraftingScene : MonoBehaviour
 {
+    public GameObject statusText;
 
     private GameObject background;
     private float ingredientWidth;
@@ -14,7 +16,9 @@ public class CraftingScene : MonoBehaviour
     private Toggle staffToggle;
     private Toggle bombToggle;
     private Toggle potionToggle;
-    private List<Toggle> ingredientToggles; //list to keep track of which items are toggled
+    private List<UIItem> ingredients; //list to keep track of which items are toggled
+    private GameObject currentStatusText;
+    private GameObject newCraftWindow;
 
     void Start()
     {
@@ -23,11 +27,8 @@ public class CraftingScene : MonoBehaviour
         canvasScale = GameObject.Find("Canvas").transform.localScale.x;
         background = GameObject.Find("Background");
         topLeftCorner = new Vector2(-516f * canvasScale, 361.8f * canvasScale);
-        ingredientToggles = new List<Toggle>(6*6);
-        for (int i = 0; i < 6*6; i++)
-        {
-            ingredientToggles.Add(null);
-        }
+        ingredients = new List<UIItem>(6*6);
+        currentStatusText = null;
 
         GlobalData.Instance.AddTestItems();
         populateStandardItems();
@@ -40,43 +41,80 @@ public class CraftingScene : MonoBehaviour
         Vector3 staffPos = background.transform.position;
         staffPos.x -= 780f * canvasScale;
         staffPos.y += 298.6f * canvasScale;
-        GameObject stick = (GameObject)Instantiate(Resources.Load("Prefabs/UI/Stick"), staffPos, Quaternion.identity, background.transform);
-        staffToggle = stick.GetComponent<Toggle>();
+        GameObject staff = (GameObject)Instantiate(Resources.Load("Prefabs/UI/Stick"), staffPos, Quaternion.identity, background.transform);
+        staffToggle = staff.GetComponent<Toggle>();
+        staff.AddComponent<EventTrigger>();
+        EventTrigger.Entry staffEnterEvent = new EventTrigger.Entry();
+        staffEnterEvent.eventID = EventTriggerType.PointerEnter;
+        staffEnterEvent.callback.AddListener((data) => { StaffPointerEnter((PointerEventData)data); });
+        EventTrigger.Entry staffExitEvent = new EventTrigger.Entry();
+        staffExitEvent.eventID = EventTriggerType.PointerExit;
+        staffExitEvent.callback.AddListener((data) => { PointerExit((PointerEventData)data); });
+        staff.GetComponent<EventTrigger>().triggers.Add(staffEnterEvent);
+        staff.GetComponent<EventTrigger>().triggers.Add(staffExitEvent);
         
         Vector3 bombPos = background.transform.position;
         bombPos.x -= 780f * canvasScale;
         //bombPos.y += 0;
         GameObject bomb = (GameObject)Instantiate(Resources.Load("Prefabs/UI/Bomb"), bombPos, Quaternion.identity, background.transform);
         bombToggle = bomb.GetComponent<Toggle>();
-        
+        bomb.AddComponent<EventTrigger>();
+        EventTrigger.Entry bombEnterEvent = new EventTrigger.Entry();
+        bombEnterEvent.eventID = EventTriggerType.PointerEnter;
+        bombEnterEvent.callback.AddListener((data) => { BombPointerEnter((PointerEventData)data); });
+        EventTrigger.Entry bombExitEvent = new EventTrigger.Entry();
+        bombExitEvent.eventID = EventTriggerType.PointerExit;
+        bombExitEvent.callback.AddListener((data) => { PointerExit((PointerEventData)data); });
+        bomb.GetComponent<EventTrigger>().triggers.Add(bombEnterEvent);
+        bomb.GetComponent<EventTrigger>().triggers.Add(bombExitEvent);
+
         Vector3 potionPos = background.transform.position;
         potionPos.x -= 780f * canvasScale;
         potionPos.y -= 300f * canvasScale;
         GameObject potion = (GameObject)Instantiate(Resources.Load("Prefabs/UI/Potion"), potionPos, Quaternion.identity, background.transform);
         potionToggle = potion.GetComponent<Toggle>();
+        potion.AddComponent<EventTrigger>();
+        EventTrigger.Entry potionEnterEvent = new EventTrigger.Entry();
+        potionEnterEvent.eventID = EventTriggerType.PointerEnter;
+        potionEnterEvent.callback.AddListener((data) => { PotionPointerEnter((PointerEventData)data); });
+        EventTrigger.Entry potionExitEvent = new EventTrigger.Entry();
+        potionExitEvent.eventID = EventTriggerType.PointerExit;
+        potionExitEvent.callback.AddListener((data) => { PointerExit((PointerEventData)data); });
+        potion.GetComponent<EventTrigger>().triggers.Add(potionEnterEvent);
+        potion.GetComponent<EventTrigger>().triggers.Add(potionExitEvent);
     }
 
-    private void populateIngredients(List<Item> ingredients) {
+    private void populateIngredients(List<Item> ingredientList) {
 
         //spawn ingredient-type items on ui
-        Debug.Log("Drawing " + ingredients.Count + " ingredients.");
+        Debug.Log("Drawing " + ingredientList.Count + " ingredients.");
         for (int row = 0; row < 6; row++)
         {
             for (int col = 0; col < 6; col++)
             {
                 int index = 6 * row + col;
-                if (index >= ingredients.Count)
+                if (index >= ingredientList.Count)
                 {
                     //gone through all ingredients
                     goto PostLoop;
                 }
                 //put item on ui
                 Vector3 pos = background.transform.position;
-                pos.x += topLeftCorner.x  + col * ingredientWidth * canvasScale;
-                pos.y += topLeftCorner.y - row * ingredientHeight * canvasScale;
-                string resourcePath = "Prefabs/UI/" + ingredients[index].name.Replace(" ", string.Empty);
+                Vector2 itemPos = getItemPosition(row, col);
+                pos.x += itemPos.x;
+                pos.y += itemPos.y;
+                string resourcePath = "Prefabs/UI/" + ingredientList[index].name.Replace(" ", string.Empty);
                 GameObject ingredient = (GameObject)Instantiate(Resources.Load(resourcePath), pos, Quaternion.identity, background.transform);
-                ingredientToggles[index] = ingredient.GetComponent<Toggle>();
+                ingredients.Add(new UIItem(ingredientList[index], ingredient.GetComponent<Toggle>()));
+                ingredient.AddComponent<EventTrigger>();
+                EventTrigger.Entry ingredientEnterEvent = new EventTrigger.Entry();
+                ingredientEnterEvent.eventID = EventTriggerType.PointerEnter;
+                ingredientEnterEvent.callback.AddListener((data) => { IngredientPointerEnter((PointerEventData)data); });
+                EventTrigger.Entry ingredientExitEvent = new EventTrigger.Entry();
+                ingredientExitEvent.eventID = EventTriggerType.PointerExit;
+                ingredientExitEvent.callback.AddListener((data) => { PointerExit((PointerEventData)data); });
+                ingredient.GetComponent<EventTrigger>().triggers.Add(ingredientEnterEvent);
+                ingredient.GetComponent<EventTrigger>().triggers.Add(ingredientExitEvent);
             }
         }
     PostLoop:
@@ -84,16 +122,43 @@ public class CraftingScene : MonoBehaviour
         Debug.Log("all items on UI.");   
     }
 
+    private Vector2 getItemPosition(int row, int col)
+    {
+        return new Vector2(topLeftCorner.x + col * ingredientWidth * canvasScale, topLeftCorner.y + -row * ingredientWidth * canvasScale);
+    }
+
+    public void OnPointerEnter(PointerEventData data)
+    {
+        Debug.Log("Pointer enter!");
+    }
+
     private void CraftItem(Item item)
     {
         if (item == null)
         {
-            //display 
+            //display
+            Debug.Log("Crafting null");
             return;
         }
-        
-        //Display item crafted
 
+        newCraftWindow = (GameObject)Instantiate(Resources.Load("Prefabs/UI/NewCraftBackground"), this.transform.position, Quaternion.identity, this.transform);
+
+        //spawn ingredient
+        Vector3 itemPos = newCraftWindow.transform.position;
+        itemPos.x -= 319f * canvasScale;
+        itemPos.y += 135.3f * canvasScale;
+        string resourcePath = "Prefabs/UI/" + item.name.Replace(" ", string.Empty);
+        //string resourcePath = "Prefabs/UI/MammothBone";
+        GameObject ingredient = (GameObject)Instantiate(Resources.Load(resourcePath), itemPos, Quaternion.identity, newCraftWindow.transform);
+        
+        //spawn flavortext x y w h 85.5 -4.4 548.2 
+        Vector3 textPos = newCraftWindow.transform.position;
+        textPos.x += 85.6f * canvasScale;
+        textPos.y -= 4.6f * canvasScale;
+        GameObject flavorText = (GameObject)Instantiate(Resources.Load("Prefabs/UI/FlavorText"), textPos, Quaternion.identity, newCraftWindow.transform);
+        flavorText.GetComponent<Text>().text = item.flavorText;
+
+        Debug.Log("Crafting " + item.name);
 
         if (!GlobalData.Instance.HasItemID(item.index, item.cat))
         {
@@ -118,13 +183,28 @@ public class CraftingScene : MonoBehaviour
         }
         if (b)
         {
-
+            if (itemIDs.Contains(3) && itemIDs.Count == 1)
+            {
+                CraftItem(new Item(0, ItemCategory.Bomb));
+            }
         }
         if (p)
         {
             if (itemIDs.Contains(3) && itemIDs.Count == 1) 
             {
                 CraftItem(new Item(0, ItemCategory.Potion)); //Chilled Drink
+            }
+            if (itemIDs.Contains(7) && itemIDs.Count == 1)
+            {
+                CraftItem(new Item(4, ItemCategory.Potion)); // sniffle enhancer
+            }
+            if (itemIDs.Contains(5) && itemIDs.Count == 1)
+            {
+                CraftItem(new Item(1, ItemCategory.Potion)); //Curry sauce
+            }
+            if (itemIDs.Contains(3) && itemIDs.Contains(8) && itemIDs.Count == 2)
+            {
+                CraftItem(new Item(5, ItemCategory.Potion)); //Mushroom proof lotion
             }
         }
         CraftItem(null);
@@ -135,12 +215,112 @@ public class CraftingScene : MonoBehaviour
         HashSet<int> itemIDs = new HashSet<int>();
         for (int i = 0; i < items.Count; i++)
         {
-            if (ingredientToggles[i] != null && ingredientToggles[i].isOn)
+            if (ingredients[i] != null && ingredients[i].toggle.isOn)
             {
                 itemIDs.Add(items[i].index);
             }
         }
         return itemIDs;
+    }
+
+    public void StaffPointerEnter(PointerEventData data)
+    {
+        Destroy(currentStatusText);
+        //spawn status-text
+        Vector3 pos = staffToggle.gameObject.transform.position;
+        pos.x += 77.5f * canvasScale;
+        pos.y -= 106f * canvasScale;
+        currentStatusText = (GameObject)Instantiate(statusText, pos, Quaternion.identity, staffToggle.gameObject.transform);
+        currentStatusText.GetComponentInChildren<Text>().text = "Staff";
+        Debug.Log("Pointer enter");
+    }
+
+    public void BombPointerEnter(PointerEventData data)
+    {
+        Destroy(currentStatusText);
+        //spawn status-text
+        Vector3 pos = bombToggle.gameObject.transform.position;
+        pos.x += 77.5f * canvasScale;
+        pos.y -= 106f * canvasScale;
+        currentStatusText = (GameObject)Instantiate(statusText, pos, Quaternion.identity, bombToggle.gameObject.transform);
+        currentStatusText.GetComponentInChildren<Text>().text = "Bomb";
+        Debug.Log("Pointer enter");
+    }
+
+    public void PotionPointerEnter(PointerEventData data)
+    {
+        Destroy(currentStatusText);
+        //spawn status-text
+        Vector3 pos = potionToggle.gameObject.transform.position;
+        pos.x += 77.5f * canvasScale;
+        pos.y -= 106f * canvasScale;
+        currentStatusText = (GameObject)Instantiate(statusText, pos, Quaternion.identity, potionToggle.gameObject.transform);
+        currentStatusText.GetComponentInChildren<Text>().text = "Potion";
+        Debug.Log("Pointer enter");
+    }
+
+    public void IngredientPointerEnter(PointerEventData data)
+    {
+        Toggle toggle = null;
+        foreach (GameObject g in data.hovered)
+        {
+            if (g.tag == "Ingredient")
+            {
+                toggle = g.GetComponentInParent<Toggle>();
+                break;
+            }
+        }
+        if (toggle == null)
+        {
+            Debug.Log("Couldnt find toggle of mouseover.");
+            return;
+        }
+        //spawn status-text
+        Vector3 pos = toggle.gameObject.transform.position;
+        pos.x += 77.5f * canvasScale;
+        pos.y -= 106f * canvasScale;
+        currentStatusText = (GameObject)Instantiate(statusText, pos, Quaternion.identity, toggle.gameObject.transform);
+        currentStatusText.GetComponentInChildren<Text>().text = "Potion";
+        currentStatusText.GetComponent<RectTransform>().SetAsLastSibling();
+        currentStatusText.transform.parent.gameObject.GetComponent<RectTransform>().SetAsLastSibling();
+        //find which item
+        Item item = null;
+        foreach(UIItem i in ingredients)
+        {
+            if (i.toggle == toggle)
+            {
+                item = i.item;
+            }
+        }
+        if (item == null)
+        {
+            Debug.Log("For some reason failed to find item.");
+            return;
+        }
+        Text textField = currentStatusText.GetComponentInChildren<Text>();
+        textField.text = item.name;
+        if (item.name.Length > 5)
+        {
+            textField.fontSize -= 7;
+        }
+        if (item.name.Length > 7)
+        {
+            textField.fontSize -= 5;
+        }
+        if (item.name.Length > 12)
+        {
+            textField.fontSize -= 3;
+        }
+    }
+
+    public void PointerExit(PointerEventData data)
+    {
+        Destroy(currentStatusText);
+    }
+
+    public void PointerDown()
+    {
+        Destroy(newCraftWindow);
     }
 
     public void Exit()
---
+++ Assets/Scripts/UIItem.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UIItem {
+    public Item item;
+    public Toggle toggle;
+
+    public UIItem(Item item, Toggle toggle)
+    {
+        this.item = item;
+        this.toggle = toggle;
+    }
+}
